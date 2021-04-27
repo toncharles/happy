f6f66b7ddadf4401cdc0e50c07151f1ceceeaa66
Washington Charles <toncharles@live.com>
2021-04-26 23:11:28 -0300

    layout orphanagesMap

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,6 +1,32 @@
+import mapMakerImg from '../images/map-marker.svg';
+import {Link} from 'react-router-dom';
+import {FiPlus} from 'react-icons/fi';
+
+import '../styles/pages/orphanages-map.css';
+
 function OrphanagesMap() {
   return (
-    <h1>Hello World</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMakerImg} alt="happy"/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Goiânia</strong>
+          <span>Goiás</span>
+        </footer>
+      </aside>
+
+      <div></div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#ffff" />
+      </Link>
+    </div>
   )
 }
 
