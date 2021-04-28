9035df39a3f5606e47d51f6bad7b9760c76e90a9
Washington Charles <toncharles@live.com>
2021-04-28 20:15:04 -0300

    exception handling

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import errorHandler from './errors/handler';
 import path from 'path';
 import './database/connection';
 import routes from './routes';
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler)
 
 
 app.listen(3333);
