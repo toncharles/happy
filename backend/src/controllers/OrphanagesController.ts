f8574bbf58b0cc86cbd8c0595cce1ffb6455f8db
Washington Charles <toncharles@live.com>
2021-04-28 16:04:50 -0300

    image upload

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -10,6 +10,15 @@ export default {
     return response.json(orphanages);
   },
 
+  async show(request: Request, response: Response) {
+    const {id} = request.params;
+
+    const orphanagesRepository = getRepository(Orphanage);
+    const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+    return response.json(orphanage);
+  },
+
   async create(request: Request, response: Response) {
     const {
       name,
@@ -22,6 +31,10 @@ export default {
     } = request.body;
   
     const orphanagesRepository = getRepository(Orphanage);
+    const requestImages = request.files as Express.Multer.File[];
+    const images = requestImages.map(image => {
+      return {path: image.filename}
+    })
     const orphanage = orphanagesRepository.create({
       name,
       latitude,
@@ -29,7 +42,8 @@ export default {
       about,
       instructions,
       opening_hours,
-      open_on_weekends
+      open_on_weekends,
+      images
     });
   
     await orphanagesRepository.save(orphanage);
