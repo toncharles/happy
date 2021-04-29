eb610adf91c55fac3cfb3af4191fd3aff7ca1576
Washington Charles <toncharles@live.com>
2021-04-29 19:46:17 -0300

    cors inclusion

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import 'express-async-errors';
 import errorHandler from './errors/handler';
 import path from 'path';
+import cors from 'cors';
 import './database/connection';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
