d7dd7f13ecfc681198be99c1bbfa39baae4d7617
Washington Charles <toncharles@live.com>
2021-04-29 11:54:00 -0300

    OrphanagesMap Finish

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,10 +1,20 @@
-import mapMakerImg from '../images/map-marker.svg';
 import {Link} from 'react-router-dom';
-import {FiPlus} from 'react-icons/fi';
-import {MapContainer, TileLayer} from 'react-leaflet';
+import {FiArrowRight, FiPlus} from 'react-icons/fi';
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import Leaflet from 'leaflet';
 
+import mapMakerImg from '../images/map-marker.svg';
 import '../styles/pages/orphanages-map.css';
-import 'leaflet/dist/leaflet.css'
+import 'leaflet/dist/leaflet.css';
+
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMakerImg,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor:[170, 0]
+});
 
 function OrphanagesMap() {
   return (
@@ -29,9 +39,22 @@ function OrphanagesMap() {
         style={{width: '100%', height: '100%'}}
       >
         <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+        <Marker 
+          icon={mapIcon} 
+          position={[-16.6799799,-49.2577286]} 
+        >
+          <Popup className="map-popup" closeButton= {false} minWidth={240} maxWidth={240} >
+            Teste
+            <Link to="/orphanages/1">
+              <FiArrowRight size={20} color="#fff" />
+            </Link>
+          </Popup>
+        </Marker>
+
       </MapContainer>
 
-      <Link to="/map" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#ffff" />
       </Link>
     </div>
